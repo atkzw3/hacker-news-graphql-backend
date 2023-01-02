cf75d12ac41a9c95f917c8e707b48acc8ad8aaa6
atkzw3 <akiyoshi.t3@gmail.com>
2023-01-02 14:02:09 +0900

    :sparkles: add database connect

---
+++ src/script.js
@@ -0,0 +1,26 @@
+// データベースにアクセスするためのクライアントライブラリ
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main(){
+  // prismaの後のリンクは、モデルを参照している。Link => link ex) User => prisma.userとなる
+  const newLink = await prisma.link.create({
+    data: {
+      description: "猫はとてもかわいい生き物です",
+      url: "こちらはURLです",
+    }
+  })
+
+  const allLinks = await prisma.link.findMany();
+  console.log("allLinks", allLinks);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+  // データベース接続を閉じる
+  prisma.$disconnect();
+})
