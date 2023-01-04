9958e70c3ba3e86f7d08340e2bfc9527af0b0c34
atkzw3 <akiyoshi.t3@gmail.com>
2023-01-04 14:51:41 +0900

    :sparkles: connect prisma via server

--- src/server.js
+++ src/server.js
@@ -3,40 +3,27 @@ const { ApolloServer, gql } = require('apollo-server');
 const fs = require("fs");
 const path = require("path");
 
-// HackerNewsの配列
-const links = [
-  {
-    id: 'links-0',
-    description: 'GraphQLの説明です。',
-    url: 'https://news.ycombinator.com/'
-  },
-  {
-    id: 'links-1',
-    description: 'GraphQLの説明2です。',
-    url: 'https://cyounkins.medium.com/encrypted-dns-ntp-deadlock-9e378940b79f'
-  },
-]
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // リゾルバ関数
 // graphQLスキーマ定義で定義した型に対して値を入れる
 const resolvers = {
   Query: {
     info: () => 'HackerNews クローン',
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
 
   Mutation: {
-    createNews: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-
-      links.push(link);
-      return link;
+    createNews: (parent, args, context) => {
+      return context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description
+        },
+      });
     }
   }
 };
@@ -45,7 +32,10 @@ const resolvers = {
 // https://www.apollographql.com/docs/apollo-server/v2/getting-started
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
-  resolvers
+  resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({url}) => {
